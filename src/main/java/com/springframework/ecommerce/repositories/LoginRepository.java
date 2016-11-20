8eb83897fbdf1685fe4a964f0af8748fc0f9d0d2
narendraraghu <narendraraghu.89@gmail.com>
2016-11-20 21:38:19 +0530

    new origin commit

--- src/main/java/com/springframework/ecommerce/repositories/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springframework.ecommerce.repositories;
-
-import com.springframework.ecommerce.domain.Login;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LoginRepository extends MongoRepository<Login, String> {
-
-    public Login findByUsername(String username);
-}
